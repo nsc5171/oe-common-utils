0dc68da1ba2a6ea2c698b65762534442c02127f9
Sai Chandra N <nsaichandra55@gmail.com>
2021-02-07 03:30:08 +0530

    AppConfig and utils

---
+++ utils/misc-utils.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = {
+    valueAt: function valueAt(target, path, fallbackVal) {
+        let op = target;
+        let steps;
+        if (Array.isArray(path)) {
+            steps = path;
+        } else {
+            steps = typeof path === 'string' ? path.split('.') : [];
+        }
+        try {
+            steps.forEach(s => op = op[s]);
+        } catch (err) {
+            op = fallbackVal;
+        }
+        return op === undefined ? fallbackVal : op;
+    },
+    arrayify: function arrayify(inp) {
+        return inp === undefined ? [] : Array.isArray(inp) ? inp : [inp];
+    }
+}
