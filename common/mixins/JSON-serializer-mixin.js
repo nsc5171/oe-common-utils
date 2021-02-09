b5713fa778f01abf4c6de21801681537047ba0fd
Sai Chandra N <nsaichandra55@gmail.com>
2021-02-09 17:09:02 +0530

    serialization extensions

--- common/mixins/JSON-serializer-mixin.js
+++ common/mixins/JSON-serializer-mixin.js
@@ -4,6 +4,12 @@ module.exports = function JSONSerializerMixin(Model) {
 
     Model.prototype._toJSONDataSerializationAdapter = function _toJSONDataSerializationAdapter(options, next) {
         let self = this;
+        self._seriablization_changes = { _modifiedAtSrlzn: 'Y' };
+        process.nextTick(next);
     }
+
+    Model.observe('loaded',(ctx,next)=>{
+        process.nextTick(next);
+    })
 
 }
