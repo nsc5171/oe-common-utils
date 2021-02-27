f0a5692c0f9d51dd47ec64b907e20b16f5d67163
Sai Chandra N <nsaichandra55@gmail.com>
2021-02-28 00:14:49 +0530

    gcloud logging option support

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 'use strict';
 
-const modeBaseClassWrapper = require('./server/wrappers/toJSON-wrapper.js')
+const modeBaseClassWrapper = require('./server/wrappers/toJSON-wrapper.js');
+const logger = require('oe-logger');
+const utils = require('./utils');
+
+function gcloudLoggerEnablement() {
+    let loggerCfg = utils.safeParse(process.env.LOGGER_CONFIG);
+    if (typeof loggerCfg !== 'object') return;
+    let streams = utils.arrayify(loggerCfg.logStreams);
+    let gcloudLogger = streams.find(i => i.type === 'gcloud-logging');
+    if (gcloudLogger) {
+        logger('LOGGER-CONFIG').streams.push(new (require('@google-cloud/logging-bunyan').LoggingBunyan)().stream(gcloudLogger.level || 'error'));
+    }
+    return true;
+}
+
+gcloudLoggerEnablement();
+
 
 module.exports = function (app) {
 };
