0c317cfb0b746278949403201358731e08d41471
Sai Chandra N <nsaichandra55@gmail.com>
2021-02-28 04:45:43 +0530

    gcloud logging

--- utils/gcloud.js
+++ utils/gcloud.js
@@ -16,7 +16,11 @@ module.exports = {
             if ([true, "TRUE", "true", "Y"].some(v => v === process.env.ENABLE_GCLOUD_LOGGING)) {
                 let stream = new (require('@google-cloud/logging-bunyan').LoggingBunyan)().stream(levels[process.env.ENABLE_GCLOUD_LOGGING_LVL] || 50);
                 stream.raw = true;
-                oeLogger('LOGGER-CONFIG').getLogger().streams.push(stream);
+                oeLogger('LOGGER-CONFIG').getLogger().addStream({
+                    name: "nsc-oe-gc-logs",
+                    stream,
+                    level: levels[process.env.ENABLE_GCLOUD_LOGGING_LVL] ? process.env.ENABLE_GCLOUD_LOGGING_LVL : "error"
+                });
             }
             loggingChecked = true;
         }
