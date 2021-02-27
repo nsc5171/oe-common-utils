01abf9f40f1161aba566a6cc368eeec616a7e1ec
Sai Chandra N <nsaichandra55@gmail.com>
2021-02-28 00:57:01 +0530

    gcloud logging fixes

--- utils/gcloud.js
+++ utils/gcloud.js
@@ -6,7 +6,7 @@ module.exports = {
             if (loggingChecked) return;
             if (!oeLogger) oeLogger = require('oe-logger');
             if ([true, "TRUE", "true", "Y"].some(v => v === process.env.ENABLE_GCLOUD_LOGGING)) {
-                logger('LOGGER-CONFIG').getLogger().streams.push(new (require('@google-cloud/logging-bunyan').LoggingBunyan)().stream(process.env.ENABLE_GCLOUD_LOGGING_LVL || 'error'));
+                oeLogger('LOGGER-CONFIG').getLogger().streams.push(new (require('@google-cloud/logging-bunyan').LoggingBunyan)().stream(process.env.ENABLE_GCLOUD_LOGGING_LVL || 'error'));
             }
             loggingChecked = true;
         }
