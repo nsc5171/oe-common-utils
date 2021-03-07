630084caf7577ac07c0cc09f9b8065f24f0eccbc
Sai Chandra N <nsaichandra55@gmail.com>
2021-03-07 16:40:54 +0530

    lb-utils

--- utils/lb-utils.js
+++ utils/lb-utils.js
@@ -3,10 +3,16 @@
 const miscUtils = require('./misc-utils');
 
 module.exports = {
-    modelUtils: {
+    modelUtils: Object.assign((() => {
+        try {
+            return require('loopback-datasource-juggler/lib/utils') || {};
+        } catch (err) {
+            return {};
+        }
+    })(), {
         getRemoteMethodsOfModel: function getRemoteMethodsOfModel(model) {
             if (typeof model === 'string') model = miscUtils.lb.findModel(model);
             return miscUtils.arrayify(miscUtils.valueAt(model, 'sharedClass._methods'));
         }
-    }
+    })
 }
